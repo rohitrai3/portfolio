ac7e94772763c6d44b0dd3097c394039e586aee5
rohitrai <raistudying@gmail.com>
2023-01-21 15:47:11 +0530

    Header.tsx: insert social icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import { SocialIcon } from 'react-social-icons';
+import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
-  <header>
-    <div>
-      {/* Social Icons */}
-      <SocialIcon url="https://twitter.com/jaketrent" />
-    </div>
-  </header>
+    <header>
+      <div className="flex flex-row items-center">
+        {/* Social Icons */}
+        <SocialIcon
+          url="https://github.com/rohitrai3"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/rohitrai03/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="rohitraijob@gmail.com"
+          fgColor="gray"
+          bgColor="transparent"
+          label="Email"
+        />
+      </div>
+    </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import { Inter } from '@next/font/google';
-import Header from '../components/Header';
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+import Header from "../components/Header";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
