b7d03db1e89468ef866d4882148a91006b21a66c
rohitrai <raistudying@gmail.com>
2023-01-21 15:58:53 +0530

    Header.tsx: center all items

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header>
+    <header className="sticky top-0 pd-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <div className="flex flex-row items-center">
         {/* Social Icons */}
         <SocialIcon
@@ -27,7 +27,7 @@ export default function Header({}: Props) {
           fgColor="gray"
           bgColor="transparent"
         />
-        <p className="uppercase hiddern md:inline-flex text-sm text-gray-400">
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
           Get In Touch
         </p>
       </div>
