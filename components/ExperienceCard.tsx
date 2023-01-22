98cf324b863a9099653521f9a3e2a49fb7ef1274
rohitrai <raistudying@gmail.com>
2023-01-22 13:27:44 +0530

    ExperienceCard.tsx: add motion to compamy's logo

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 export default function ExperienceCard({}: Props) {
   return (
     <article>
-      <img
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        viewport={{
+          once: true,
+        }}
         className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
         src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/logo.png"
         alt="Company Logo"
