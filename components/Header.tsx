287675032b49f7d688a506d0d48bd8593f63623c
rohitrai <raistudying@gmail.com>
2023-01-22 08:43:10 +0530

    Header.tsx: animate Get In Touch

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,7 +36,22 @@ export default function Header({}: Props) {
         />
       </motion.div>
 
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
         <SocialIcon
           className="cursor-pointer"
           network="email"
@@ -46,7 +61,7 @@ export default function Header({}: Props) {
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
           Get In Touch
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 }
