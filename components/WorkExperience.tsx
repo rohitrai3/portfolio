b040f98973be4de98c98d3ec26691a995698c4c8
rohitrai <raistudying@gmail.com>
2023-01-22 19:48:32 +0530

    remove duplicate scrollbar

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -22,7 +22,7 @@ export default function WorkExperience({}: Props) {
         Work Experience
       </h3>
 
-      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80">
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80">
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
