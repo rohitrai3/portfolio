af9445984f34c99ff0f77c870186471e72f82a45
rohitrai <raistudying@gmail.com>
2023-01-22 14:45:06 +0530

    Skills.tsx: style heading

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,5 +3,11 @@ import React from "react";
 type Props = {};
 
 export default function Skills({}: Props) {
-  return <div>Skills</div>;
+  return (
+    <div className="flex relative flex-col text-center md:textpleft xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Skills
+      </h3>
+    </div>
+  );
 }
