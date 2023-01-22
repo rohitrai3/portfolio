07ced11a2e1eaa982f3fba89a30c1730d6499c25
rohitrai <raistudying@gmail.com>
2023-01-22 17:47:34 +0530

    Projects.tsx: add motion to main section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
@@ -7,7 +8,18 @@ export default function Projects({}: Props) {
   const projects = [1, 2, 3, 4, 5];
 
   return (
-    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1.5,
+      }}
+      className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
@@ -45,6 +57,6 @@ export default function Projects({}: Props) {
       </div>
 
       <div className="w-full absolute top-[30%] bg-[#f7ab0a]/10 left-0 h-[500px] -skew-y-12" />
-    </div>
+    </motion.div>
   );
 }
