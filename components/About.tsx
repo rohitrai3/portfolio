ae81af37fc5e2d188f30a0d05a5f2df0445ca35f
rohitrai <raistudying@gmail.com>
2023-01-22 11:46:00 +0530

    About.tsx: add motion to mine photo

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
@@ -8,6 +9,25 @@ export default function About({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
+
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{
+          opacity: 1,
+          x: 0,
+        }}
+        viewport={{
+          once: true,
+        }}
+        src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/logo.png"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
+      />
     </div>
   );
 }
