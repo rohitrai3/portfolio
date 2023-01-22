e2741dbcd9b98d354f0545c8ad522ec0876967b0
rohitrai <raistudying@gmail.com>
2023-01-22 09:59:22 +0530

    BackgroundCircles.tsx: styled 5 circles

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 export default function BackgroundCircles({}: Props) {
-  return <div>BackgroundCircles</div>;
+  return (
+    <div className="relative flex justify-center items-center">
+      <div className="absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
+      <div className="absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52" />
+      <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52" />
+      <div className="absolute border border-[#f7ab0a] rounded-full h-[650px] w-[650px] mt-52 animate-pulse opacity-20" />
+      <div className="absolute border border-[#333333] rounded-full h-[800px] w-[800px] mt-52" />
+    </div>
+  );
 }
