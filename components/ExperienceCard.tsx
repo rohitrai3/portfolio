bd7e89b1adf99086f6b64900d19fcf4e15c507f9
rohitrai <raistudying@gmail.com>
2023-01-22 13:56:52 +0530

    ExperienceCard.tsx: style main section

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 export default function ExperienceCard({}: Props) {
   return (
-    <article>
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
       <motion.div
         initial={{
           y: -100,
