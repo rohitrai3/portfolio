9122dcfc6ea461e10b6815002ec9a0300180f408
rohitrai <raistudying@gmail.com>
2023-01-22 11:14:35 +0530

    Hero.tsx: put tag line and buttons on top of circles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,7 @@ export default function Hero({}: Props) {
         width={128}
         height={128}
       />
-      <div>
+      <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
           Software Development Engineer
         </h2>
