ee6e38387e3f914f2a2f205bd4275fd409b9fd46
rohitrai <raistudying@gmail.com>
2023-01-22 17:43:53 +0530

    Projects.tsx: project details

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -29,6 +29,16 @@ export default function Projects({}: Props) {
                 </span>{" "}
                 PhotoShare
               </h4>
+
+              <p className="text-lg text-center md:text-left">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
             </div>
           </div>
         ))}
