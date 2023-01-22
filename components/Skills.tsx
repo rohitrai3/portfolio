ed4f9da2a0df057ad2ae2cef6f1ec3c2f1011cb2
rohitrai <raistudying@gmail.com>
2023-01-22 14:55:03 +0530

    Skills.tsx: add motion

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Skill from "./Skill";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 export default function Skills({}: Props) {
   return (
-    <div className="flex relative flex-col text-center md:textpleft xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1.5,
+      }}
+      className="flex relative flex-col text-center md:textpleft xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
       </h3>
@@ -32,6 +44,6 @@ export default function Skills({}: Props) {
         <Skill />
         <Skill />
       </div>
-    </div>
+    </motion.div>
   );
 }
