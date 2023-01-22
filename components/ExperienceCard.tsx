f06b9e4483db482998e7ece1dd4de3d4d09dd4ad
rohitrai <raistudying@gmail.com>
2023-01-22 13:25:04 +0530

    ExperienceCard.tsx: add company logos

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -3,5 +3,13 @@ import React from "react";
 type Props = {};
 
 export default function ExperienceCard({}: Props) {
-  return <div>ExperienceCard</div>;
+  return (
+    <article>
+      <img
+        className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+        src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/logo.png"
+        alt="Company Logo"
+      />
+    </article>
+  );
 }
