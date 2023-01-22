76a57a86ed3629e2a76c22ee8aa7eafab695c817
rohitrai <raistudying@gmail.com>
2023-01-22 15:36:19 +0530

    Skill.tsx: show proficiency on hover

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -30,6 +30,11 @@ export default function Skill({ directionLeft }: Props) {
           height={128}
         />
       </motion.div>
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 rounded-full z-0">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+        </div>
+      </div>
     </div>
   );
 }
