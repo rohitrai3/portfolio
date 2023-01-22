a67c8d62291a300e5128b208cf845e62efabe478
rohitrai <raistudying@gmail.com>
2023-01-22 18:01:19 +0530

    ContactMe.tsx: style main section

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -3,5 +3,9 @@ import React from "react";
 type Props = {};
 
 export default function ContactMe({}: Props) {
-  return <div>ContactMe</div>;
+  return (
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      ContactMe
+    </div>
+  );
 }
