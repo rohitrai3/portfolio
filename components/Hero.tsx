81e14440fa1671c3d040cf8e25979695efe716ca
rohitrai <raistudying@gmail.com>
2023-01-22 09:28:23 +0530

    Hero.tsx: typewriter effect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 
 type Props = {};
 
 export default function Hero({}: Props) {
-  return <div>Hero</div>;
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The Name's Rohit Rai",
+      "Guy-who-loves-Coffe.tsx",
+      "<ButLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div>
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor="#f7ab0a" />
+      </h1>
+    </div>
+  );
 }
