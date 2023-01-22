162dbdd4a51e333d2e66af9fde9251cfcb732914
rohitrai <raistudying@gmail.com>
2023-01-22 15:05:07 +0530

    Skill.tsx: add tech stack logos

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 type Props = {};
 
 export default function Skill({}: Props) {
-  return <div className="group relative flex cursor-pointer">Skill</div>;
+  return (
+    <div className="group relative flex cursor-pointer">
+      <Image
+        src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/logo.png"
+        className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:graysacle transition duration-300 ease-in-out"
+        width={96}
+        height={96}
+        alt="Tech logo"
+      />
+    </div>
+  );
 }
