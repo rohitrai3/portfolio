f62e06b8107ae3f5a7a8df59886ad24815243554
rohitrai <raistudying@gmail.com>
2023-01-22 12:57:02 +0530

    WorkExperience.tsx: style section

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -3,5 +3,9 @@ import React from "react";
 type Props = {};
 
 export default function WorkExperience({}: Props) {
-  return <div>WorkExperience</div>;
+  return (
+    <div className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
+      WorkExperience
+    </div>
+  );
 }
