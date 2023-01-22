7242292dd092ac134585211308726189bf9e97b7
rohitrai <raistudying@gmail.com>
2023-01-22 12:49:58 +0530

    WorkExperience.tsx: create and import

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from "@next/font/google";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import WorkExperience from "../components/WorkExperience";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,7 +27,10 @@ export default function Home() {
         <About />
       </section>
 
-      {/* Experience */}
+      {/* Work Experience */}
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
 
       {/* Skills */}
 
