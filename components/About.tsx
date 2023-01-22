4b923c73dd4fc2ef9b7dbdd1cc485d43eb101d4d
rohitrai <raistudying@gmail.com>
2023-01-22 11:27:00 +0530

    About.tsx: style component

--- components/About.tsx
+++ components/About.tsx
@@ -3,5 +3,9 @@ import React from "react";
 type Props = {};
 
 export default function About({}: Props) {
-  return <div>About</div>;
+  return (
+    <div className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      About
+    </div>
+  );
 }
