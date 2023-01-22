d59982863fc138ed8faf20e87b316dfa0e386233
rohitrai <raistudying@gmail.com>
2023-01-22 17:52:50 +0530

    Projects.tsx: add motion to project logo

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -27,12 +27,29 @@ export default function Projects({}: Props) {
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
         {projects.map((project, i) => (
           <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-            <Image
-              src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/logo.png"
-              alt="Project photo"
-              width={128}
-              height={128}
-            />
+            <motion.div
+              initial={{
+                y: -300,
+                opacity: 0,
+              }}
+              transition={{
+                duration: 1.2,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+              }}
+              viewport={{
+                once: true,
+              }}
+            >
+              <Image
+                src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/logo.png"
+                alt="Project photo"
+                width={128}
+                height={128}
+              />
+            </motion.div>
 
             <div className="space-y-10 px-0 md:px-10 max-w-6xl">
               <h4 className="text-4xl font-semibold text-center">
