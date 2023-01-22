4ab26dab5d227f2545b3786c2e6ed851ec640495
rohitrai <raistudying@gmail.com>
2023-01-22 16:22:38 +0530

    Projects.tsx: style main section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,5 +3,9 @@ import React from "react";
 type Props = {};
 
 export default function Projects({}: Props) {
-  return <div>Projects</div>;
+  return (
+    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+      Projects
+    </div>
+  );
 }
