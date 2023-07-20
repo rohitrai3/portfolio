d795893c606b8d1535f755a946706a1734573517
Rohit Rai <raistudying@gmail.com>
2023-07-20 17:01:41 +0530

    Update experience section

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import About from "./about";
-import Experience from "./experience/experience";
+import Experience from "./experience";
 import Home from "./home";
 import Navigation, { NavigationProps } from "./navigation";
 import { useState } from "react";
