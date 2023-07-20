5a5798283c1075cfc3045b717c0a19f39231b541
Rohit Rai <raistudying@gmail.com>
2023-07-20 11:50:38 +0530

    Add profile photo in home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,8 +1,10 @@
+import ProfilePhoto from "../../assets/rohit_rai.jpg";
+
 export default function Home() {
   return (
     <div className="home">
       <div className="home-heading">
-        <img src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/logo.png" />
+        <img className="profile-photo" src={ProfilePhoto} />
         <div className="tagline-small on-background-text">
           SOFTWARE ENGINEER
         </div>
