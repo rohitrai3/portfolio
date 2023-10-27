a1dafb72e52fede9a9c6e893195b318b2aad655f
Rohit Rai <rohitraijob@gmail.com>
2023-10-27 11:47:57 +0530

    feat: hover effect on links

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -3,11 +3,11 @@ import data from "./data.json";
 
 export default function Projects() {
   return (
-    <div className="max-w-lg lg:ml-dvw-1/2 max-lg:self-center mt-20 mx-10 space-y-10">
-      <h1 className="uppercase text-4xl text-center">Projects</h1>
+    <div className="max-w-lg lg:ml-dvw-1/2 max-lg:self-center mt-20 mx-10">
+      <h1 className="uppercase text-4xl text-center mb-10">Projects</h1>
       {Object.values(data).map((val) => (
         <a className="block" href={val.link}>
-          <div className="space-y-2">
+          <div className="space-y-2 hover:scale-105 hover:bg-gray-900 p-5 hover:border-0 hover:rounded-xl">
             <img width={val.width} src={val.logo} alt={val.name} />
             <h2 className="text-2xl">{val.name}</h2>
             <ul className="list-disc ml-5">
--- src/components/socials/index.tsx
+++ src/components/socials/index.tsx
@@ -4,10 +4,10 @@ export default function Socials() {
   return (
     <div className="max-w-lg lg:ml-dvw-1/2 max-lg:self-center my-20 mx-10 space-y-10">
       <h1 className="uppercase text-4xl text-center">Socials</h1>
-      <div className="grid grid-cols-3 gap-x-10">
+      <div className="flex justify-between">
         {Object.values(data).map((val) => (
           <a href={val.link}>
-            <div className="grid gap-1">
+            <div className="grid hover:scale-105 hover:bg-gray-900 p-5 hover:border-0 hover:rounded-xl">
               <img
                 className="h-24 justify-self-center"
                 src={val.logo}
